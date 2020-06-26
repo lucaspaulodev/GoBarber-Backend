e5435f63c08dc83de58b509c4a679d3dbff3eaab
Lucas Paulo <lucaspaulodev@gmail.com>
2020-06-26 13:59:18 -0300

    Database updates

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import { parseISO} from 'date-fns'
+import { getCustomRepository } from 'typeorm'
 
 
 
@@ -7,21 +8,24 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository'
 import CreateAppointmentsServices from '../services/CreateAppointmentService'
 
 const appointmentsRouter = Router()
-const appointmentsRepository = new AppointmentsRepository()
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all()
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+  const appointments = await appointmentsRepository.find()
   return response.json(appointments)
 })
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try{
     const { provider, date } = request.body
-    const parsedDate = parseISO(date)
-    const createAppointment = new CreateAppointmentsServices(appointmentsRepository)
-    const appointment = createAppointment.execute({provider, date: parsedDate})
-    return response.json(appointment)
 
+    const parsedDate = parseISO(date)
+
+    const createAppointment = new CreateAppointmentsServices()
+
+    const appointment = await createAppointment.execute({provider, date: parsedDate})
+
+    return response.json(appointment)
   } catch(err){
     return response.status(400).json({ error: err.message })
   }
