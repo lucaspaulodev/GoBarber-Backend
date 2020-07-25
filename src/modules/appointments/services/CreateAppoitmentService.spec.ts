ea0958523abee445d7343179aeb09b9f7c848781
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-25 18:25:17 -0300

    Add validation appointment date test

--- src/modules/appointments/services/CreateAppoitmentService.spec.ts
+++ src/modules/appointments/services/CreateAppoitmentService.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
 import CreateAppointmentService from './CreateAppointmentService'
+import AppError from '@shared/errors/AppError'
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -16,5 +17,19 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123123')
   })
 
-// it('should not be able to create a new appointment on the same date', () => {})
+   it('should not be able to create a new appointment on the same date', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository()
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository)
+    const appointmentDate = new Date(2020, 4, 10, 11)
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123'
+    })
+
+    expect(createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123'
+    })).rejects.toBeInstanceOf(AppError)
+   })
 })
