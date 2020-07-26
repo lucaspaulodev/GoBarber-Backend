caafae3a507a549049eb3ae24f2cc3c6aea55aae
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-26 15:46:48 -0300

    Fixing some iddentations

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -11,7 +11,10 @@ describe('CreateUser', () => {
     const fakeUsersRepository = new FakeUsersRepository()
     const fakeHashProvider = new FakeHashProvider()
 
-    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    )
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -25,7 +28,10 @@ describe('CreateUser', () => {
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
     const fakeHashProvider = new FakeHashProvider()
-    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    )
 
     await createUser.execute({
       name: 'John Doe',
