c753c34b95bdb4f094bdedb51cd4ccfaf88b6c70
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-27 20:59:27 -0300

    Adding recover password tests

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,8 +1,9 @@
 import {injectable, inject} from 'tsyringe'
 
-// import AppError from '@shared/errors/AppError'
+import AppError from '@shared/errors/AppError'
 import IUsersRepository from '@modules/users/repositories/IUserRepository'
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
+import IUserTokensRepository from '../repositories/IUserTokensRepository'
 
 // import User from '../infra/typeorm/entities/User'
 
@@ -17,9 +18,19 @@ class SendForgotPasswordEmailService {
     private usersRepository: IUsersRepository,
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({email}: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user){
+      throw new AppError('User does not exists.')
+    }
+
+    await this.userTokensRepository.generate(user.id)
+
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido'
