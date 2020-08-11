801ff2b19965cdc447a6f5547e6b28064ec7c501
Lucas Paulo <lucaspaulodev@gmail.com>
2020-08-11 16:49:30 -0300

    Refactoring tests to notification service

--- src/modules/appointments/services/CreateAppoitmentService.spec.ts
+++ src/modules/appointments/services/CreateAppoitmentService.spec.ts
@@ -1,15 +1,23 @@
-import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
-import CreateAppointmentService from './CreateAppointmentService'
+import 'reflect-metadata'
+
 import AppError from '@shared/errors/AppError'
 
+import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository'
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
+
+let fakeNotificationsRepository: FakeNotificationsRepository
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
 let createAppointment: CreateAppointmentService;
 
 describe('CreateAppointment', () => {
   beforeEach(()=>{
     fakeAppointmentsRepository = new FakeAppointmentsRepository()
-    createAppointment = new CreateAppointmentService(fakeAppointmentsRepository)
+    fakeNotificationsRepository = new FakeNotificationsRepository()
+
+    createAppointment = new CreateAppointmentService(fakeAppointmentsRepository, fakeNotificationsRepository)
   })
+
   it('should be able to create a new appointment', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
       return new Date(2020, 4, 10, 12).getTime()
@@ -26,7 +34,11 @@ describe('CreateAppointment', () => {
   })
 
    it('should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date(2020, 4, 10, 11)
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020,4, 10, 10).getTime()
+    })
+
+    const appointmentDate = new Date(2020, 4, 10, 12)
 
     await createAppointment.execute({
       date: appointmentDate,
