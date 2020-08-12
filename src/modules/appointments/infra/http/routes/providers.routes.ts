e7391935905fe41777781411e3b161f90b246888
Lucas Paulo <lucaspaulodev@gmail.com>
2020-08-12 14:47:05 -0300

    Validating routes with celebrate

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import {celebrate, Segments, Joi} from 'celebrate'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 
@@ -14,7 +15,16 @@ const providerDayAvailabilityController = new ProviderDayAvailabilityController(
 providersRouter.use(ensureAuthenticated)
 
 providersRouter.get('/', providersController.index)
-providersRouter.get('/:id/month-availability', providerMonthAvailabilityController.index)
-providersRouter.get('/:id/day-availability', providerDayAvailabilityController.index)
+providersRouter.get('/:id/month-availability', celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  }
+}),providerMonthAvailabilityController.index)
+
+providersRouter.get('/:id/day-availability', celebrate({
+  [Segments.PARAMS]: {
+    provider_id: Joi.string().uuid().required(),
+  }
+}), providerDayAvailabilityController.index)
 
 export default providersRouter
