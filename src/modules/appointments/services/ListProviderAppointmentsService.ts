a129eaa07c9aa7ac923efbf67937552138cac586
Lucas Paulo <lucaspaulodev@gmail.com>
2020-08-14 15:10:05 -0300

    Setuping Redis and CacheProvider

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import {injectable, inject} from 'tsyringe'
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
 import Appointment from '../infra/typeorm/entities/Appointment'
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 // import User from '@modules/users/infra/typeorm/entities/User'
 
 interface IRequest{
@@ -15,16 +16,24 @@ interface IRequest{
 class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({provider_id, year, month, day}: IRequest): Promise<Appointment[]>{
+    const cacheData = await this.cacheProvider.recover('');
+
+    console.log(cacheData);
+
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       day,
       month,
       year,
     })
+
+    await this.cacheProvider.save('', '');
 
     return appointments;
   }
