af0f087ce442245aacbd7ccbd7b866e3adffff47
Lucas Paulo <lucaspaulodev@gmail.com>
2020-09-01 10:54:07 -0300

    Fixing ProvidersController and ListProvidersService

--- src/modules/appointments/services/ListProvidersServices.ts
+++ src/modules/appointments/services/ListProvidersServices.ts
@@ -4,6 +4,7 @@ import IUsersRepository from '@modules/users/repositories/IUserRepository'
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 
 import User from '@modules/users/infra/typeorm/entities/User'
+import { classToClass } from 'class-transformer'
 
 interface IRequest{
   user_id: string;
@@ -26,7 +27,7 @@ class ListProvidersService{
         except_user_id: user_id
       })
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users)
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users))
     }
 
     return users
